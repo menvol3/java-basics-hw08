f562e6d41fb24c98051c40df574f0cc6eb0a6992
menvol3 <disbalance@ukr.net>
2020-03-24 22:54:50 +0200

    Tests passed

--- src/RegExp.java
+++ src/RegExp.java
@@ -6,18 +6,19 @@ import java.util.regex.Pattern;
  */
 public class RegExp {
 
-  /**
-   * Get IP address from text.
-   * @param text text to parse
-   * @return String with IP address
-   */
-  public static String getIpAddress(String text) {
-    Pattern ip = Pattern.compile("");
-    Matcher matcher = ip.matcher(text);
-    if(matcher.find()){
-      return matcher.group();
-    }else {
-    return "";
+    /**
+     * Get IP address from text.
+     *
+     * @param text text to parse
+     * @return String with IP address
+     */
+    public static String getIpAddress(String text) {
+        Pattern ip = Pattern.compile("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+        Matcher matcher = ip.matcher(text);
+        if (matcher.find()) {
+            return matcher.group();
+        } else {
+            return "";
+        }
     }
-  }
 }
--- src/RegExpTest.java
+++ src/RegExpTest.java
@@ -5,27 +5,28 @@ import org.testng.annotations.Test;
 
 public class RegExpTest {
 
-  /**
-   * Test data.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "text")
-  public static Object[][] text() {
-    return new Object[][]{
-        {"8.8.8.8", "8.8.8.8"},
-        {"http://192.168.1.1/index.html", "192.168.1.1"},
-        {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
-        {"IP could not have negative numbers like -1.-2.-3.-4", ""},
-        {" Invalid IP: 300.300.300.300!", ""}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "text")
+    public static Object[][] text() {
+        return new Object[][]{
+                {"8.8.8.8", "8.8.8.8"},
+                {"http://192.168.1.1/index.html", "192.168.1.1"},
+                {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
+                {"IP could not have negative numbers like -1.-2.-3.-4", ""},
+                {" Invalid IP: 300.300.300.300!", ""},
+                {" My ip address is 10.0.1.0", "10.0.1.0"},
+                {"Here is wrong ip address 192.168.256.8", ""}
+        };
+    }
 
-  @Test(dataProvider = "text")
-  public void testSumArray(String text, String expectedResult) {
-    String actualResult = RegExp.getIpAddress(text);
+    @Test(dataProvider = "text")
+    public void testSumArray(String text, String expectedResult) {
+        String actualResult = RegExp.getIpAddress(text);
 
-    assertEquals(actualResult, expectedResult, "IP address is not correct");
-  }
+        assertEquals(actualResult, expectedResult, "IP address is not correct");
+    }
 }
